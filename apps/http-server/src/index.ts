8dbacd31d75a80fd83521971b38615252dea128c
Subhajit578 <123349280+Subhajit578@users.noreply.github.com>
2026-02-03 12:56:22 -0600

    Pushing http Server

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,25 @@
+import  express  from "express";
+import {client} from "@repo/db/client"
+import "dotenv/config";
+ const app = express();
+ app.use(express.json())
+ app.get("/" , (req,res) => {
+    res.send("Hi There")
+ })
+ app.post("/signup", async function (req, res) {
+    const username = req.body.username
+    const password = req.body.password
+    console.log(req.body)
+    const user = await client.user.create({
+        data : {
+            username :username, 
+            password : password
+        }
+    })
+    res.json({
+        message : "Signup Sucessful", 
+        id : user.id
+    })
+ })
+ app.listen(3001)
+
